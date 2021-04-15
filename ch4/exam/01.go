d0d4f51a4993cc995be78e7e9770c818e308d157
HyunSang Park <hyun.sang@parkhyunsang.com>
2021-04-15 20:22:24 +0900

    [UPDATE🚀] 연습 문제

---
+++ ch4/exam/01.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a int = 3
+	var b float32 = 3.1415
+	var c string = "Hello World"
+	var d int32 = int32(10)
+	var e float32 = 3.1415
+
+	fmt.Println(a)
+	fmt.Println(b)
+	fmt.Println(c)
+	fmt.Println(d)
+	fmt.Println(e)
+}
---
+++ ch4/exam/02.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a int32 = 360
+	var b int8 = int8(a)
+
+	fmt.Println(b)
+}
