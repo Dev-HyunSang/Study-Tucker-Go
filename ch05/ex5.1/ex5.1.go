74a776514b2b8652a5f88614e40372e5c1eef706
HyunSang Park <hyun.sang@parkhyunsang.com>
2021-04-16 19:31:05 +0900

    [UPDATE🚀] Package fmt

---
+++ ch05/ex5.1/ex5.1.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a int = 10
+	var b int = 20
+	var f float64 = 32799438743.8297
+
+	fmt.Println("a:", a, "b:", b)
+	fmt.Println("a:", b, "b:", b, "f:", f)
+	fmt.Println("a: %d b: %d f:%f\n", a, b, f)
+
+}
