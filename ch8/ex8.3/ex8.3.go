8283725b6af7cbacd2319522d2ef658d5aa2c399
HyunSang Park <hyun.sang@parkhyunsang.com>
2021-04-24 14:33:57 +0900

    [UPDATE🚀] 상수는 언제 사용하나? / 코드값으로 사용하기

---
+++ ch8/ex8.3/ex8.3.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+const Pig int = 0
+const Cow int = 1
+const Chicken int = 2
+
+func PrintAnimal(animal int) {
+	if animal == Pig {
+		fmt.Println("꿀꿀")
+	} else if animal == Cow {
+		fmt.Println("음메")
+	} else if animal == Chicken {
+		fmt.Print("꼬끼오")
+	} else {
+		fmt.Println("...")
+	}
+}
+
+func main() {
+	PrintAnimal(Pig)
+	PrintAnimal(Cow)
+	PrintAnimal(Chicken)
+}
