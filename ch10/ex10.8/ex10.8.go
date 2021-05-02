6a095ef80c713d426b152fc7b81d3bd7580c9937
HyunSang Park <hyun.sang@parkhyunsang.com>
2021-05-02 18:40:32 +0900

    [UPDATE🚀] 다양한 switch문 형태 / const 열거값과 switch

---
+++ ch10/ex10.8/ex10.8.go
@@ -0,0 +1,35 @@
+package main
+
+import "fmt"
+
+type ColorType int
+
+const (
+	Red ColorType = iota
+	Blue
+	Green
+	Yellow
+)
+
+func colorToString(color ColorType) string {
+	switch color {
+	case Red:
+		return "Red"
+	case Blue:
+		return "Blue"
+	case Green:
+		return "Green"
+	case Yellow:
+		return "Yellow"
+	default:
+		return "Undefined"
+	}
+}
+
+func getMyFavoriteColor() ColorType {
+	return Blue
+}
+
+func main() {
+	fmt.Println("My favorite Color is", colorToString(getMyFavoriteColor()))
+}
