91398c71c804a78d5776f4d16a986f709b4e7625
HyunSang Park <hyun.sang@parkhyunsang.com>
2021-05-18 22:19:26 +0900

    [UPDATE🚀] 문자열 / []rune 타입 변환으로 글자 수 알아내기

---
+++ ch15/ex15.5/ex15.5.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	str := "Hello, World"
+
+	runes := []rune{72, 101, 108, 108, 111, 32, 87, 111, 114, 108, 100}
+
+	fmt.Println(str)
+	fmt.Println(string(runes))
+}
