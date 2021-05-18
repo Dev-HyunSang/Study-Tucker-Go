051ef9bcdf7f07711f0c79671a4c2e11a31ad335
HyunSang Park <hyun.sang@parkhyunsang.com>
2021-05-18 21:10:51 +0900

    [UPDATE🚀] 문자열 / 문자열

---
+++ ch15/ex15.2/ex15.2.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	poet1 := "죽는 날까지 하늘을 우러러\n한 점 부끄럼이 없기를,\n잎새에 이는 바람에도\n나는 괴로워했다.\n"
+
+	poet2 := `죽늘 날까지 하늘을 우러러
+한 점 부끄럼이 없기를,
+잎새에 이는 바람에도
+나는 괴로워했다.`
+
+	fmt.Println(poet1)
+	fmt.Println(poet2)
+}
