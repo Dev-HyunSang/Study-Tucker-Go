c38e2c7ae6bed008b57628a81fbc1c82a47c013a
HyunSang Park <hyun.sang@parkhyunsang.com>
2021-05-19 09:24:07 +0900

    [FIX📦] 오타 코드 변경

--- ch15/ex15.6/ex15.6.go
+++ ch15/ex15.6/ex15.6.go
@@ -6,6 +6,6 @@ func main() {
 	str := "Hello 월드"
 	runes := []rune(str)
 
-	fmt.Printf("len(str) = %d\n", len(runes))
+	fmt.Printf("len(str) = %d\n", len(str))
 	fmt.Printf("len(runes) = %d\n", len(runes))
 }
