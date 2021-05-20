ccdfdc9d219366a69f4534a7009bdeda5b842799
HyunSang Park <hyun.sang@parkhyunsang.com>
2021-05-20 18:08:52 +0900

    [UPDATE🚀] 유용한 슬라이싱 기능 활용 / 요소 삭제

---
+++ ch18/ex18.10/ex18.10.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	slice := []int{1, 2, 3, 4, 5, 6}
+	idx := 2
+
+	for i := idx + 1; i < len(slice); i++ {
+		slice[i-1] = slice[i]
+	}
+	slice = slice[:len(slice)-1]
+	fmt.Println(slice)
+}
