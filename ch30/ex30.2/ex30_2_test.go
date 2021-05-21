d1d9826d8c71ef3bdd7d0db535d6ee9b819a29e8
HyunSang Park <hyun.sang@parkhyunsang.com>
2021-05-21 23:35:55 +0900

    [UPDATE🚀]

---
+++ ch30/ex30.2/ex30_2_test.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"encoding/json"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestJsonHandler(t *testing.T) {
+	assert := assert.New(t)
+
+	res := httptest.NewRecorder()
+	req := httptest.NewRequest("GET", "/students", nil)
+
+	mux := MakeWebHandler()
+	mux.ServeHTTP(res, req)
+
+	assert.Equal(http.StatusOK, res.Code)
+	var list []Student
+	err := json.NewDecoder(res.Body).Decode(&list)
+	assert.Nil(err)
+	assert.Equal(2, len(list))
+	assert.Equal("aaa", list[0].Name)
+	assert.Equal("bbb", list[1].Name)
+}
+
+func TestJsonHandler2(t *testing.T) {
+	assert := assert.New(t)
+
+	var student Student
+	mux := MakeWebHandler()
+	res := httptest.NewRecorder()
+	req := httptest.NewRequest("GET", "/students/1", nil)
+
+	mux.ServeHTTP(res, req)
+	assert.Equal(http.StatusOK, res.Code)
+	err := json.NewDecoder(res.Body).Decode(&student)
+	assert.Nil(err)
+	assert.Equal("aaa", student.Name)
+
+	res = httptest.NewRecorder()
+	req = httptest.NewRequest("GET", "/students/2", nil)
+	mux.ServeHTTP(res, req)
+	assert.Equal(http.StatusOK, res.Code)
+	err = json.NewDecoder(res.Body).Decode(&student)
+	assert.Nil(err)
+	assert.Equal("bbb", student.Name)
+}
