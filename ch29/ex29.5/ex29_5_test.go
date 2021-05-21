0e106ba62460ced1545eccf03ccd5099d486e49b
HyunSang Park <hyun.sang@parkhyunsang.com>
2021-05-21 13:18:45 +0900

    [UPDATE] 웹 서버 태스트 코드 만들기

---
+++ ch29/ex29.5/ex29_5_test.go
@@ -0,0 +1,39 @@
+//ch29/ex29.5/ex29_5_test.go
+package main
+
+import (
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestIndexHandler(t *testing.T) {
+	assert := assert.New(t)
+
+	res := httptest.NewRecorder()
+	req := httptest.NewRequest("GET", "/", nil)
+
+	mux := MakeWebHandler()
+	mux.ServeHTTP(res, req)
+
+	assert.Equal(http.StatusOK, res.Code) /
+	data, _ := io.ReadAll(res.Body)
+	assert.Equal("Hello World", string(data))
+}
+
+func TesBarHandler(t *testing.T) {
+	assert := assert.New(t)
+
+	res := httptest.NewRecorder()
+	req := httptest.NewRequest("GET", "/bar", nil)
+
+	mux := MakeWebHandler()
+	mux.ServeHTTP(res, req)
+
+	assert.Equal(http.StatusOK, res.Code)
+	data, _ := io.ReadAll(res.Body)
+	assert.Equal("Hello Bar", string(data))
+}
