bc820fcbcd42e3633935d77500b689506da78590
HyunSang Park <hyun.sang@parkhyunsang.com>
2021-05-21 12:14:18 +0900

    [UPDATE🚀] 파일 서버 / 특정 경로에 있는 파일 읽어오기

--- ch29/ex29.4/ex29.4.go
+++ ch29/ex29.4/ex29.4.go
@@ -4,5 +4,7 @@ import "net/http"
 
 func main() {
 	http.Handle("/", http.FileServer(http.Dir("static")))
+	// 특정 경로에 있는 파일 읽어오기
+	//http.HandleFunc("/static/", http.FileServer(http.Dir("static")))
 	http.ListenAndServe(":3000", nil)
 }
