333b1c4e88de88578d5f1a5533d7be288f5968c5
HyunSang Park <hyun.sang@parkhyunsang.com>
2021-05-27 16:14:17 +0900

    [UPDATE🚀] 함수 리터럴 / 함수 리터럴 내부 상태 주의점

---
+++ ch21/ex21.6/ex21.6.go
@@ -0,0 +1,37 @@
+package main
+
+import "fmt"
+
+func CaptureLoop() {
+	f := make([]func(), 3)
+	fmt.Println("ValueLoop")
+	for i := 0; i < 3; i++ {
+		f[i] = func() {
+			fmt.Println(i)
+		}
+	}
+
+	for i := 0; i < 3; i++ {
+		f[i]()
+	}
+}
+
+func CaptureLoop2() {
+	f := make([]func(), 3)
+	fmt.Println("ValueLoop2")
+	for i := 0; i < 3; i++ {
+		v := i
+		f[i] = func() {
+			fmt.Println(v)
+		}
+	}
+
+	for i := 0; i < 3; i++ {
+		f[i]()
+	}
+}
+
+func main() {
+	CaptureLoop()
+	CaptureLoop2()
+}
